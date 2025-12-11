77f38803097efe558d03b87c4a48eddf1ccab712
xenixsolus <hemantkumarsonare@gmail.com>
2025-12-11 16:37:42 +0530

    Fixing Bug

--- components/GoogleLoginButton.jsx
+++ components/GoogleLoginButton.jsx
@@ -6,6 +6,8 @@ import { saveToken } from '@/lib/auth';
 export default function GoogleLoginButton({ onSuccess }) {
   console.log("ENV CLIENT:", process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID);
 
+  const BASE_URL = process.env.NEXT_PUBLIC_APP_URL;
+
   useEffect(() => {
     function init() {
       if (!window.google) return;
@@ -14,7 +16,7 @@ export default function GoogleLoginButton({ onSuccess }) {
       client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
       callback: handle,
       ux_mode: "popup",
-      login_uri: "http://localhost:3000/login",
+      login_uri: `${BASE_URL}/login`,
       auto_select: false,
     });
 
